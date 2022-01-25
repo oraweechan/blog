6a87c0e0d40e8d7aa28e6e95a8982063132db59a
oraweechan <89260194+oraweechan@users.noreply.github.com>
2022-01-25 11:21:49 -0800

    create main-navigation and logo components

---
+++ components/home-page/featured-posts.js
@@ -0,0 +1,5 @@
+import classes from './'
+
+function FeaturedPosts() {}
+
+export default FeaturedPosts;
---
+++ components/layout/logo.js
@@ -0,0 +1,9 @@
+import classes from "./logo.module.css";
+
+function Logo() {
+    return(
+        <div>Orawee's Blog</div>
+    )
+}
+
+export default Logo;
