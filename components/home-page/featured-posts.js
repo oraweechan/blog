3e947584c7b2095ac40512f9cf5d0cf90f26bec8
oraweechan <89260194+oraweechan@users.noreply.github.com>
2022-01-25 20:07:23 -0800

    added a post grid and post items

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,5 +1,11 @@
-import classes from './'
+import classes from './featured-posts.module.css'
 
-function FeaturedPosts() {}
+function FeaturedPosts() {
+    return(
+        <section className={classes.latest}>
+            <h2>Featured Posts</h2>
+        </section>
+    )
+}
 
 export default FeaturedPosts;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import { Fragment } from 'react';
+import FeaturedPosts from '../components/home-page/featured-posts';
 import Hero from '../components/home-page/hero';
 
 function HomePage() {
     return (
         <Fragment>
         <Hero />
+        <FeaturedPosts />
         </Fragment>
     )           
 }
