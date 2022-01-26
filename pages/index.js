6ca246408033b5a70efb81846da5e8540feb3d7e
oraweechan <89260194+oraweechan@users.noreply.github.com>
2022-01-25 22:53:48 -0800

    added All Posts page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,45 @@
-import { Fragment } from 'react';
-import FeaturedPosts from '../components/home-page/featured-posts';
-import Hero from '../components/home-page/hero';
+import { Fragment } from "react";
+import FeaturedPosts from "../components/home-page/featured-posts";
+import Hero from "../components/home-page/hero";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "getting-started-with-nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS is the React framework for production",
+    date: "2022-01-25",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "getting-started-with-nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS is the React framework for production",
+    date: "2022-01-25",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "getting-started-with-nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS is the React framework for production",
+    date: "2022-01-25",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "getting-started-with-nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS is the React framework for production",
+    date: "2022-01-25",
+  },
+];
 
 function HomePage() {
-    return (
-        <Fragment>
-        <Hero />
-        <FeaturedPosts />
-        </Fragment>
-    )           
+  return (
+    <Fragment>
+      <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
+    </Fragment>
+  );
 }
 
 export default HomePage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,40 @@
-function AllPostsPage() {}
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+    {
+      slug: "getting-started-with-nextjs",
+      title: "getting-started-with-nextjs",
+      image: "getting-started-nextjs.png",
+      excerpt: "NextJS is the React framework for production",
+      date: "2022-01-25",
+    },
+    {
+      slug: "getting-started-with-nextjs2",
+      title: "getting-started-with-nextjs",
+      image: "getting-started-nextjs.png",
+      excerpt: "NextJS is the React framework for production",
+      date: "2022-01-25",
+    },
+    {
+      slug: "getting-started-with-nextjs3",
+      title: "getting-started-with-nextjs",
+      image: "getting-started-nextjs.png",
+      excerpt: "NextJS is the React framework for production",
+      date: "2022-01-25",
+    },
+    {
+      slug: "getting-started-with-nextjs4",
+      title: "getting-started-with-nextjs",
+      image: "getting-started-nextjs.png",
+      excerpt: "NextJS is the React framework for production",
+      date: "2022-01-25",
+    },
+  ];
+
+function AllPostsPage() {
+    return (
+        <AllPosts posts={DUMMY_POSTS} />
+    )
+}
 
 export default AllPostsPage;
