6cd4b1b6e01ee29991b4310466d24529d5f0fe4d
oraweechan <89260194+oraweechan@users.noreply.github.com>
2022-01-27 22:09:32 -0800

    stored messages with mongodb in a database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,23 +1,56 @@
-function handler (req,res) {
-    if (req.method === 'POST') {
-        const { email, name, message } = req.body;
+import { MongoClient } from "mongodb";
 
-        if (!email || !email.includes('@') || !name || name.trim() ===  '' || !message ||
-        message.trim() === '') {
-            res.status(422).json({ message: 'Invalid input.' });
-            return;
-        }
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
 
-        // store it in a database
-        const newMessage = {
-            email,
-            name,
-            message
-        };
-        console.log(newMessage);
-
-        res.status(201).json( {message: 'Successfully stored message!', message: newMessage });
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input." });
+      return;
     }
+
+    // store it in a database
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+    // console.log(newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://testUser:test@nextjsblog.waulz.mongodb.net/my-site?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    }
+
+    client.close();
+
+    res
+      .status(201)
+      .json({ message: "Successfully stored message!", message: newMessage });
+  }
 }
 
 export default handler;
